7dc7cc52e4104c16f2c3fcf545de7a17398857bf
Willy-Kuo <willy0731@gmail.com>
2025-03-11 16:36:31 +0800

    load

---
+++ PPT.py
@@ -0,0 +1,6 @@
+from pptx import Presentation
+from pptx.util import Inches
+
+def createPPT():
+    prs = Presentation()
+    
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import time
 import base64
 from io import BytesIO
 from PIL import Image
+import os
+import PPT
 
 ACCOUNT = ""
 PASSWORD = ""
@@ -25,30 +27,47 @@ myCourse_form = driver.find_element(By.PARTIAL_LINK_TEXT, "我的課程")
 myCourse_form.click()
 driver.implicitly_wait(10)
 
-def Get_course_list():
-    course_class = driver.find_element(By.NAME, "s_sys")
-    print(course_class[0])
-# Get_course_list()
-driver.switch_to.frame("s_main")
-course_elements_list = [div_element.find_element(By.TAG_NAME, "a") for div_element in driver.find_elements(By.TAG_NAME, "div") if div_element.get_attribute("class")=="text-left"][1:]  #TAG a
-course_name_list = [element.get_attribute("text") for element in course_elements_list]
-course_elements_list[2].click()
+def Get_course_names_and_course_elements():
+    course_elements = [div_element.find_element(By.TAG_NAME, "a") for div_element in driver.find_elements(By.TAG_NAME, "div") if div_element.get_attribute("class")=="text-left"][1:]
+    course_names = [element.get_attribute("text") for element in course_elements]
+    return course_elements, course_names
 
-time.sleep(3)
-# print([element.get_attribute("class") for element in driver.find_elements(By.TAG_NAME, "div")])
-viewer_element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID, "viewer"))
-)
-canva_elements = [element for element in viewer_element.find_elements(By.TAG_NAME, "div") if element.get_attribute("class")=="canvasWrapper"]
-canva_element = canva_elements[1]
-canvas_base64 = driver.execute_script("""
-    var canvas = arguments[0].querySelector("canvas");
-    return canvas.toDataURL("image/png");
-""", canva_element)
-driver.quit()
+while True:
+    driver.switch_to.parent_frame()
+    driver.switch_to.frame("s_main")
+    course_elements, course_names = Get_course_names_and_course_elements()
+    time.sleep(2)
+    try:
+        os.system("cls")
+        for n, name in enumerate(course_names):
+            print(f"[{n}]{name}", end='  ')
+        print()
+        choose_course_index = int(input("Enter number:"))
+        course_elements[choose_course_index].click()
+    except:
+        continue
+
+    time.sleep(5)
+    viewer_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "viewer"))
+    )
+    canva_elements = [element for element in viewer_element.find_elements(By.TAG_NAME, "div") if element.get_attribute("class")=="canvasWrapper"]
+    canva_element = canva_elements[1]
+    canvas_base64 = driver.execute_script("""
+        var canvas = arguments[0].querySelector("canvas");
+        return canvas.toDataURL("image/png");
+    """, canva_element)
 
 
+    canvas_bytes = base64.b64decode(canvas_base64.split(",")[1])
+    image = Image.open(BytesIO(canvas_bytes))
+    image.save("image.png")
 
-canvas_bytes = base64.b64decode(canvas_base64.split(",")[1])
-image = Image.open(BytesIO(canvas_bytes))
-image.save("image.png")
+    print("[0]Continue  [1]Leave")
+    Again = int(input("Enter number:"))
+    if Again:
+        break
+    driver.switch_to.parent_frame()
+    driver.switch_to.frame("s_sysbar")
+    driver.execute_script("goPersonal()")
+    time.sleep(5)
